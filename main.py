0cb2c593e72dd7bea2c17152161943d06362fd9b
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-12-09 15:11:08 -0300

    U

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from Classes.game import Person, Bcolors
-magic = [{"name": "Fire", "cost": 10, "dmg": 160},
-        {"name": "Thunder", "cost": 10, "dmg": 124},
-        {"name": "Blizzard", "cost": 10, "dmg": 100}]
+from Classes.magic import Spell
+from Classes.Inventory import Item
+# Create Black Magic
+fire = Spell ("Fire", 10, 100, "black")
+thunder = Spell ("Thunder", 10, 100, "black")
+blizzard = Spell ("Blizzard", 18, 100, "black")
+meteor = Spell ("Meteor", 20, 200, "black")
+quake = Spell ("Quake", 14, 120, "black")
 
+# white magic
+cure = Spell ("Cure", 12, 120, "white")
+cura = Spell ("Cura", 18, 200, "white")
 
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+#items
+potion = Item ("Potion", "potion","Heals 50 HP",50)
+hipotion= Item("Hi.Potion","potion","Heals 100 HP",100)
+superpotion = Item("Super Potion","potion","Heals 500 HP", 500)
+elixer= Item("Elixer","elixer","Fully restores HP/MP", 999)
+hielixer= Item("MegaElixer","elixer ","restores", 9988)
+
+granade= Item("Granade", "attack" ," deals 500 dmg", 500)
+player_spells= [fire, thunder, blizzard, meteor, quake, cure, cura]
+player_items = [potion,hipotion,superpotion,elixer,hielixer]
+#instantiate people
+player = Person(460, 65, 60, 34, player_spells, player_items)
+enemy = Person(1200, 65, 45, 25, [], [])
 running = True
 i = 0
 print(Bcolors.FAIL + Bcolors.BOLD + "AN ENEMY Attacks" + Bcolors.ENDC)
@@ -22,20 +41,32 @@ while running:
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input("Choose magic!")) -1
-        magic_dmg = player.generate_spell_Damage(magic_choice)
-        spell = player.get_spell_name(magic_choice)
-        cost = player.get_spell_mp_cpst(magic_choice)
+        if magic_choice == -1:
+            continue
+        spell=player.magic[magic_choice]
+        magic_dmg= spell.generate_damage()
+
         current_mp = player.get_mp()
-        if cost > current_mp:
+        if spell.cost > current_mp:
             print(Bcolors.FAIL + "\n Not enough MP \n" + Bcolors.ENDC)
             continue
-        player.reduce_mp(cost)
-        enemy.take_damage(magic_dmg)
-        print(Bcolors.OKBLUE + "\n" + spell + "deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
+        player.reduce_mp(spell.cost)
+        if spell.type == "white":
+            player.heal(magic_dmg)
+            print(Bcolors.OKGREEN + "\n" + spell.name + "heals for:", str(magic_dmg), "HP." + Bcolors.ENDC)
+        elif spell.type == "black":
+            enemy.take_damage(magic_dmg)
+            print(Bcolors.OKBLUE + "\n" + spell.name + "deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
 
-
-
-
+    elif index == 2:
+        player.choose_item()
+        item_choice= int(input("choose item:"))-1
+        if item_choice == -1:
+            continue
+        item = player.items[item_choice]
+        if item.type == "potion":
+            player.heal(item.prop)
+            print(Bcolors.OKGREEN+ "\n"+ item.name + "you have been healed for:", str(item.prop), "hp"+ Bcolors.ENDC)
     enemy_choice = 1
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
